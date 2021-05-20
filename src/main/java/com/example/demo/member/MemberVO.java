54de24de641ff1f926b698caef719c23c6490f48
ba-lim <dmsqltkfk276@naver.com>
2021-05-20 16:28:03 +0900

    1

--- src/main/java/com/example/demo/member/MemberVO.java
+++ src/main/java/com/example/demo/member/MemberVO.java
@@ -3,6 +3,9 @@ package com.example.demo.member;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Range;
 
 import lombok.Data;
 
@@ -12,6 +15,7 @@ public class MemberVO {
 	
 	
 	//최소 4글자 이상 통과
+	@Size(min = 4)
 	private String password;
 	@NotEmpty
 	private String name;
@@ -19,6 +23,8 @@ public class MemberVO {
 	private String email;
 	private String phone;
 	//0이상 200이하
+	@Range(min = 0, max= 200)
 	private Integer age;
-	
+	//@Max(value=200)
+	//@Min(value=0)과 같다
 }
